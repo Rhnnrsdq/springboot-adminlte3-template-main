442ab3dba3b50430fbe6cb380d91c956a3371b04
ThariqWaridi <thariq.waridi@gmail.com>
2022-06-15 15:27:27 +0700

    Initial commit

---
+++ src/main/java/com/hendisantika/controller/AdminPageController.java
@@ -0,0 +1,43 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.entity.User;
+import com.hendisantika.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-adminlte3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/11/20
+ * Time: 08.41
+ */
+@Controller
+public class AdminPageController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping({"/user/list", "/admin/user"})
+    public String listUser() {
+        return "user-list";
+    }
+
+    @GetMapping("/user/add")
+    public String showFormUser(Model model) {
+        model.addAttribute("user", new User());
+        return "user-add";
+    }
+
+    @PostMapping("/user/add")
+    public String addUser(Model model, User user) {
+        userRepository.save(user);
+        model.addAttribute("user", new User());
+        return "user-list";
+    }
+
+}
